d78ee4d550d1b9bce05abb79ad2b272c2ea065ea
Markus Kark <markus.kark@gmail.com>
2025-08-14 16:00:27 +0200

    reformatted

--- Source/AnotherWorkingTitle/AnotherWorkingTitle.Build.cs
+++ Source/AnotherWorkingTitle/AnotherWorkingTitle.Build.cs
@@ -8,9 +8,20 @@ public class AnotherWorkingTitle : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"EnhancedInput", 
+			"ProceduralMeshComponent"
+		});
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			
+		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
